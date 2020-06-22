e03804d8034da92c5644148dda8ef59b34772c78
Mike Nye <mike.nye@gmail.com>
2020-06-22 23:03:21 +0800

    Remove clamav config from master.cf (using milter instead)

--- rootfs/etc/cont-init.d/11-postfix-generate-master.cf.sh
+++ rootfs/etc/cont-init.d/11-postfix-generate-master.cf.sh
@@ -8,8 +8,3 @@ if [ "${ENABLE_SPF}" = "true" ]; then
     echo "policy  unix  -       n       n       -       0       spawn" >> "${POSTFIX_MASTERCF_FILE}"
     echo "    user=nobody argv=/usr/local/lib/policyd-spf-perl" >> "${POSTFIX_MASTERCF_FILE}"
 fi
-
-# Do we enable & configure ClamAV?
-if [ "${ENABLE_CLAMAV}" = "true" ]; then
-    echo "scan  inet  "
-fi
