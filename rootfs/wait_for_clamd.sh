1f0a41b85a35dbaddbdf5c560a793302f381db18
Mike Nye <mike.nye@gmail.com>
2020-06-23 01:18:58 +0800

    Fix by-design error

--- rootfs/wait_for_clamd.sh
+++ rootfs/wait_for_clamd.sh
@@ -4,6 +4,6 @@ echo "Waiting for clamd to become ready..."
 
 unset PONG
 while [ "$PONG" != "PONG" ]; do
-    PONG=$(echo "PING" | socat TCP-CONNECT:127.0.0.1:7358 - 2> /dev/null)
+    PONG=$(echo "PING" | socat TCP-CONNECT:127.0.0.1:7358 - 2> /dev/null) 2> /dev/null
     sleep 1
 done
