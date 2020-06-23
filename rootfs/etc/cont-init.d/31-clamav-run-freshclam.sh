09812a93ce9986b8e10dfe2a5a2d573968ef541a
Mike Nye <mike.nye@gmail.com>
2020-06-23 09:23:25 +0800

    Improve logging

--- rootfs/etc/cont-init.d/31-clamav-run-freshclam.sh
+++ rootfs/etc/cont-init.d/31-clamav-run-freshclam.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
+echo "Updating ClamAV Database..."
 if [ "${ENABLE_CLAMAV}" = "true" ]; then
     freshclam --stdout --foreground
 fi
+echo "ClamAV Database updated!"
