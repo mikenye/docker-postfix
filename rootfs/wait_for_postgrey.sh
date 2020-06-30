8b35e5fdc14b52127dbebed893038a9298096809
Mike Nye <mike.nye@gmail.com>
2020-06-30 16:17:29 +0800

    Edit wait for postgrey script

--- rootfs/wait_for_postgrey.sh
+++ rootfs/wait_for_postgrey.sh
@@ -3,7 +3,7 @@
 unset EXITCODE
 while [ "$EXITCODE" -ne "0" ]; do
     echo "Waiting for postgrey to become ready..."
-    echo "PING" | socat TCP-CONNECT:127.0.0.1:10023 -
+    echo "" | socat TCP-CONNECT:127.0.0.1:10023 -
     EXITCODE=$?
     sleep 2
 done
