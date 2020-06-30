9132e26181bb8882f56a3d0e1eea1b3d3a5d052a
Mike Nye <mike.nye@gmail.com>
2020-06-30 16:19:08 +0800

    Fix syntax error

--- rootfs/wait_for_postgrey.sh
+++ rootfs/wait_for_postgrey.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 unset EXITCODE
-while [ "$EXITCODE" -ne "0" ]; do
+while [ "$EXITCODE" -ne 0 ]; do
     echo "Waiting for postgrey to become ready..."
     echo "" | socat TCP-CONNECT:127.0.0.1:10023 -
     EXITCODE=$?
