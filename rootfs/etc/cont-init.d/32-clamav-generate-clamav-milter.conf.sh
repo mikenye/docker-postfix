9c4fc979a84d5be6b4883c784d92c2f59feb5dad
Mike Nye <mike.nye@gmail.com>
2020-07-06 12:18:05 +0800

    Prevent double-logging for clamav-milter

--- rootfs/etc/cont-init.d/32-clamav-generate-clamav-milter.conf.sh
+++ rootfs/etc/cont-init.d/32-clamav-generate-clamav-milter.conf.sh
@@ -17,7 +17,7 @@ if [ ! -z "${CLAMAV_MILTER_REPORT_HOSTNAME}" ]; then
     echo "ReportHostname ${CLAMAV_MILTER_REPORT_HOSTNAME}" >> "${CLAMAV_MILTERCONF_FILE}"
 fi
 
-echo "LogSyslog yes" >> "${CLAMAV_MILTERCONF_FILE}"
+echo "LogSyslog no" >> "${CLAMAV_MILTERCONF_FILE}"
 echo "LogRotate no" >> "${CLAMAV_MILTERCONF_FILE}"
 echo "SupportMultipleRecipients yes" >> "${CLAMAV_MILTERCONF_FILE}"
 
