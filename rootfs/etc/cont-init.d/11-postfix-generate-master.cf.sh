f1a894546d64189078923a4316b45fa6f57aa68e
Mike Nye <mike.nye@gmail.com>
2020-07-09 17:50:15 +0800

    Enable chroot operation for smtp & smtpd

--- rootfs/etc/cont-init.d/11-postfix-generate-master.cf.sh
+++ rootfs/etc/cont-init.d/11-postfix-generate-master.cf.sh
@@ -10,18 +10,24 @@ cp -v ${POSTFIX_MASTERCF_ORIGINAL_FILE} ${POSTFIX_MASTERCF_FILE}
 # Enable postscreen
 # See: http://www.postfix.org/POSTSCREEN_README.html
 # Comment out the "smtp inet ... smtpd" service in master.cf
-sed -i 's/^smtp *inet.*smtpd$/#&/' /etc/postfix/master.cf
+sed -i 's/^smtp *inet.*smtpd$/#&/' "${POSTFIX_MASTERCF_FILE}"
 # Uncomment the new "smtpd pass ... smtpd" service in master.cf
-sed -i '/^#smtpd *pass.*smtpd$/s/^#//g' /etc/postfix/master.cf
+sed -i '/^#smtpd *pass.*smtpd$/s/^#//g' "${POSTFIX_MASTERCF_FILE}"
 # Uncomment the new "smtp inet ... postscreen" service in master.cf
-sed -i '/^#smtp *inet.*postscreen$/s/^#//g' /etc/postfix/master.cf
+sed -i '/^#smtp *inet.*postscreen$/s/^#//g' "${POSTFIX_MASTERCF_FILE}"
 # Uncomment the new "tlsproxy unix ... tlsproxy" service in master.cf
-sed -i '/^#tlsproxy *unix.*tlsproxy$/s/^#//g' /etc/postfix/master.cf
+sed -i '/^#tlsproxy *unix.*tlsproxy$/s/^#//g' "${POSTFIX_MASTERCF_FILE}"
 # Uncomment the new "dnsblog unix ... dnsblog" service in master.cf
-sed -i '/^#dnsblog *unix.*dnsblog$/s/^#//g' /etc/postfix/master.cf
+sed -i '/^#dnsblog *unix.*dnsblog$/s/^#//g' "${POSTFIX_MASTERCF_FILE}"
 
 # Do we enable & configure spf-engine?
 if [ "${ENABLE_SPF}" = "true" ]; then
     echo "policy  unix  -       n       n       -       0       spawn" >> "${POSTFIX_MASTERCF_FILE}"
     echo "    user=nobody argv=/usr/local/lib/policyd-spf-perl" >> "${POSTFIX_MASTERCF_FILE}"
 fi
+
+# Enable chroot operation for smtp
+sed -i "s/smtp      inet  n       -       n       -       1       postscreen/smtp      inet  n       -       y       -       1       postscreen/" "${POSTFIX_MASTERCF_FILE}"
+
+# Enable chroot operation for smtpd
+sed -i "s/smtpd     pass  -       -       n       -       -       smtpd/smtpd     pass  -       -       y       -       -       smtpd/" "${POSTFIX_MASTERCF_FILE}"
