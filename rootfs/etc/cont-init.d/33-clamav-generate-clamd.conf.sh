d0f6a29cf2e8a70f2b65f1dcf83e099c97f32fe4
Mike Nye <mike.nye@gmail.com>
2020-07-15 14:50:35 +0800

    Add CLAMAV_CLAMD_PHISHING/HEURISTIC env vars

--- rootfs/etc/cont-init.d/33-clamav-generate-clamd.conf.sh
+++ rootfs/etc/cont-init.d/33-clamav-generate-clamd.conf.sh
@@ -13,3 +13,23 @@ echo "TCPSocket 7358" >> "${CLAMAV_CLAMDCONF_FILE}"
 echo "TCPAddr 127.0.0.1" >> "${CLAMAV_CLAMDCONF_FILE}"
 echo "User clamav" >> "${CLAMAV_CLAMDCONF_FILE}"
 echo "Foreground yes" >> "${CLAMAV_CLAMDCONF_FILE}"
+
+if [ ! -z "${CLAMAV_CLAMD_PHISHING_SIGNATURES}" ]; then
+    echo "PhishingSignatures ${CLAMAV_CLAMD_PHISHING_SIGNATURES}" >> "${CLAMAV_CLAMDCONF_FILE}"
+fi
+
+if [ ! -z "${CLAMAV_CLAMD_PHISHING_SCAN_URLS}" ]; then
+    echo "PhishingScanURLs ${CLAMAV_CLAMD_PHISHING_SCAN_URLS}" >> "${CLAMAV_CLAMDCONF_FILE}"
+fi
+
+if [ ! -z "${CLAMAV_CLAMD_PHISHING_ALWAYS_BLOCK_SSL_MISMATCH}" ]; then
+    echo "PhishingAlwaysBlockSSLMismatch ${CLAMAV_CLAMD_PHISHING_ALWAYS_BLOCK_SSL_MISMATCH}" >> "${CLAMAV_CLAMDCONF_FILE}"
+fi
+
+if [ ! -z "${CLAMAV_CLAMD_PHISHING_ALWAYS_BLOCK_CLOAK}" ]; then
+    echo "PhishingAlwaysBlockCloak ${CLAMAV_CLAMD_PHISHING_ALWAYS_BLOCK_CLOAK}" >> "${CLAMAV_CLAMDCONF_FILE}"
+fi
+
+if [ ! -z "${CLAMAV_CLAMD_HEURISTIC_SCAN_PRECEDENCE}" ]; then
+    echo "HeuristicScanPrecedence ${CLAMAV_CLAMD_HEURISTIC_SCAN_PRECEDENCE}" >> "${CLAMAV_CLAMDCONF_FILE}"
+fi
