a1f3b2dd380d9fc5ec7b252a19bf248c8e3a9290
Mike Nye <mike.nye@gmail.com>
2020-07-16 12:18:55 +0800

    Linting (SC2129)

--- rootfs/etc/cont-init.d/30-clamav-generate-freshclam.conf.sh
+++ rootfs/etc/cont-init.d/30-clamav-generate-freshclam.conf.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
-echo "" > "${CLAMAV_FRESHCLAMCONF_FILE}"
-
-echo "DatabaseDirectory /var/lib/clamav" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
-echo "LogSyslog no" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
-echo "LogRotate no" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
-echo "PidFile /run/freshclam/freshclam.pid" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
-echo "DatabaseOwner clamav" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
+{
+    echo ""
+    echo "DatabaseDirectory /var/lib/clamav"
+    echo "LogSyslog no"
+    echo "LogRotate no"
+    echo "PidFile /run/freshclam/freshclam.pid"
+    echo "DatabaseOwner clamav"
+    echo "Foreground yes"
+    echo "Bytecode yes"
+    echo "DatabaseMirror database.clamav.net"
+    echo "NotifyClamd /usr/local/etc/clamd.conf"
+} > "${CLAMAV_FRESHCLAMCONF_FILE}"
 
 if [ ! -z "${FRESHCLAM_CHECKS_PER_DAY}" ]; then
     echo "Checks ${FRESHCLAM_CHECKS_PER_DAY}" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
 fi
-
-echo "Foreground yes" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
-echo "Bytecode yes" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
-echo "DatabaseMirror database.clamav.net" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
-echo "NotifyClamd /usr/local/etc/clamd.conf" >> "${CLAMAV_FRESHCLAMCONF_FILE}"
