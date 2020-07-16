e4cb989de0a607dc3554a2e71302ec44ea8d6c3f
Mike Nye <mike.nye@gmail.com>
2020-07-16 12:07:29 +0800

    Linting (SC2236)

--- rootfs/etc/cont-init.d/20-opendkim-generate-conf.sh
+++ rootfs/etc/cont-init.d/20-opendkim-generate-conf.sh
@@ -7,12 +7,12 @@ echo "" > "${OPENDKIM_CONF_FILE}"
 echo "Syslog true" >> "${OPENDKIM_CONF_FILE}"
 
 # LogResults
-if [ ! -z "${OPENDKIM_LOGRESULTS}" ]; then
+if [ -n "${OPENDKIM_LOGRESULTS}" ]; then
     echo "LogResults true" >> "${OPENDKIM_CONF_FILE}"
 fi
 
 # LogWhy
-if [ ! -z "${OPENDKIM_LOGWHY}" ]; then
+if [ -n "${OPENDKIM_LOGWHY}" ]; then
     echo "LogWhy true" >> "${OPENDKIM_CONF_FILE}"
 fi
 
@@ -23,32 +23,32 @@ echo "Socket inet:8891@127.0.0.1" >> "${OPENDKIM_CONF_FILE}"
 echo "UserID opendkim" >> "${OPENDKIM_CONF_FILE}"
 
 # Set domains that we sign for
-if [ ! -z "${OPENDKIM_DOMAIN}" ]; then
+if [ -n "${OPENDKIM_DOMAIN}" ]; then
     echo "Domain ${OPENDKIM_DOMAIN}" >> "${OPENDKIM_CONF_FILE}"
 fi
 
 # Specify signing table
-if [ ! -z "${OPENDKIM_SIGNINGTABLE}" ]; then
+if [ -n "${OPENDKIM_SIGNINGTABLE}" ]; then
     echo "SigningTable refile:${OPENDKIM_SIGNINGTABLE}" >> "${OPENDKIM_CONF_FILE}"
 fi
 
 # Specify key file
-if [ ! -z "${OPENDKIM_KEYFILE}" ]; then
+if [ -n "${OPENDKIM_KEYFILE}" ]; then
     echo "KeyFile ${OPENDKIM_KEYFILE}" >> "${OPENDKIM_CONF_FILE}"
 fi
 
 # Specify key table
-if [ ! -z "${OPENDKIM_KEYTABLE}" ]; then
+if [ -n "${OPENDKIM_KEYTABLE}" ]; then
     echo "KeyTable refile:${OPENDKIM_KEYTABLE}" >> "${OPENDKIM_CONF_FILE}"
 fi
 
 # Set selector
-if [ ! -z "${OPENDKIM_SELECTOR}" ]; then
+if [ -n "${OPENDKIM_SELECTOR}" ]; then
     echo "Selector ${OPENDKIM_SELECTOR}" >> "${OPENDKIM_CONF_FILE}"
 fi
 
 # Set mode
-if [ ! -z "${OPENDKIM_MODE}" ]; then
+if [ -n "${OPENDKIM_MODE}" ]; then
     echo "Mode ${OPENDKIM_MODE}" >> "${OPENDKIM_CONF_FILE}"
 fi
 
@@ -59,7 +59,7 @@ echo "AutoRestart false" >> "${OPENDKIM_CONF_FILE}"
 echo "Background false" >> "${OPENDKIM_CONF_FILE}"
 
 # Set internalhosts (hosts for which to sign for)
-if [ ! -z "${OPENDKIM_INTERNALHOSTS}" ]; then
+if [ -n "${OPENDKIM_INTERNALHOSTS}" ]; then
     echo "InternalHosts ${OPENDKIM_INTERNALHOSTS}" >> "${OPENDKIM_CONF_FILE}"
 fi
 
@@ -67,6 +67,6 @@ fi
 echo "Canonicalization relaxed/relaxed" >> "${OPENDKIM_CONF_FILE}"
 
 # Sign subdomains?
-if [ ! -z "${OPENDKIM_SUBDOMAINS}" ]; then
+if [ -n "${OPENDKIM_SUBDOMAINS}" ]; then
     echo "SubDomains ${OPENDKIM_SUBDOMAINS}" >> "${OPENDKIM_CONF_FILE}"
 fi
