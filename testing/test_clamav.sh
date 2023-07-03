c7a701f6fea34540eb677b88d0b99f8299d023f2
Mike Nye <mike.nye@gmail.com>
2023-07-03 15:59:55 +0800

    testing

--- testing/test_clamav.sh
+++ testing/test_clamav.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+EXITCODE=0
+
 # exit on error
 set -e
 
@@ -10,26 +12,24 @@ echo "Wait for container start (up to 5 mins)"
 timeout 300s ./wait_for_postfix.sh
 
 echo "Attempt to send email (may fail due to greylisting)"
-./test_server.expect 127.0.0.1 2525 remote.tld infectedtester@remote.tld testuser@mail.testdomain.tld || true
+./test_infected.expect 127.0.0.1 2525 remote.tld infectedtester@remote.tld testuser@mail.testdomain.tld || true
 
 echo "Wait 5 mins for greylist timeout"
 sleep 605
 
 echo "Attempt to send email (should succeed)"
-./test_server.expect 127.0.0.1 2525 remote.tld infectedtester@remote.tld testuser@mail.testdomain.tld
+./test_infected.expect 127.0.0.1 2525 remote.tld infectedtester@remote.tld testuser@mail.testdomain.tld
 sleep 2 # wait for postfix to deliver email
 
 echo "Checking received email"
 echo ""
+docker exec mail_test touch /tmp/testuser_email
 docker exec mail_test cat /tmp/testuser_email
-docker exec mail_test cat /tmp/testuser_email | grep 'Return-Path: <infectedtester@remote.tld>' > /dev/null 2>&1
-docker exec mail_test cat /tmp/testuser_email | grep 'Delivered-To: testuser@mail.testdomain.tld' > /dev/null 2>&1
-docker exec mail_test cat /tmp/testuser_email | grep 'X-Greylist: delayed' > /dev/null 2>&1
-docker exec mail_test cat /tmp/testuser_email | grep 'Received: from remote.tld' > /dev/null 2>&1
-docker exec mail_test cat /tmp/testuser_email | grep 'From: infectedtester@remote.tld' > /dev/null 2>&1
-docker exec mail_test cat /tmp/testuser_email | grep 'To: testuser@mail.testdomain.tld' > /dev/null 2>&1
-docker exec mail_test cat /tmp/testuser_email | grep 'Subject: Infected email sent at' > /dev/null 2>&1
-docker exec mail_test cat /tmp/testuser_email | grep 'This is an infected message sent at' > /dev/null 2>&1
+if docker exec mail_test cat /tmp/testuser_email | grep 'From: infectedtester@remote.tld' > /dev/null 2>&1; then
+    EXITCODE=1
+fi
 
 echo "Stop test env"
 docker compose down
+
+exit $EXITCODE
