4ce1626347b1237b70cfc90285f32f364e564a4e
Mike Nye <mike.nye@gmail.com>
2023-07-03 15:32:21 +0800

    test script updates

--- testing/run_test.sh
+++ testing/run_test.sh
@@ -9,16 +9,17 @@ docker compose up -d
 echo "Wait for container start (up to 5 mins)"
 timeout 300s ./wait_for_postfix.sh
 
-echo "attempt to send email"
+echo "Attempt to send email (will fail due to greylisting)"
 ../test_server.expect 127.0.0.1 2525 remote.tld tester@remote.tld testuser@mail.testdomain.tld || true
 
-echo "wait for greylist timeout"
+echo "Wait 5 mins for greylist timeout"
 sleep 605
 
-echo "attempt to send email"
+echo "Attempt to send email (should succeed)"
 ../test_server.expect 127.0.0.1 2525 remote.tld tester@remote.tld testuser@mail.testdomain.tld
 
-# check received email
+echo "Checking received email"
+echo ""
 docker exec mail_test cat /tmp/testuser_email
 docker exec mail_test cat /tmp/testuser_email | grep 'Return-Path: <tester@remote.tld>' > /dev/null 2>&1
 docker exec mail_test cat /tmp/testuser_email | grep 'Delivered-To: testuser@mail.testdomain.tld' > /dev/null 2>&1
@@ -28,3 +29,6 @@ docker exec mail_test cat /tmp/testuser_email | grep 'From: tester@remote.tld' >
 docker exec mail_test cat /tmp/testuser_email | grep 'To: testuser@mail.testdomain.tld' > /dev/null 2>&1
 docker exec mail_test cat /tmp/testuser_email | grep 'Subject: Test email sent at' > /dev/null 2>&1
 docker exec mail_test cat /tmp/testuser_email | grep 'This is a test message set at date' > /dev/null 2>&1
+
+echo "Stop test env"
+docker compose down
